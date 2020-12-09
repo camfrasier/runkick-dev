61bc691229ded97cc1711dc282cd52bd15d1b7ee
Cam Frasier <frasier_cameron@yahoo.com>
2020-12-08 23:39:31 -0500

    Saving after group chat solved!

--- runkick-dev/Controller/Groups/ChatroomVC.swift
+++ runkick-dev/Controller/Groups/ChatroomVC.swift
@@ -29,7 +29,7 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
     lazy var containerView: UIView = {
         let containerView = UIView()
         containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 55)
-        containerView.backgroundColor = UIColor.rgb(red: 200, green: 200, blue: 200)
+        containerView.backgroundColor = UIColor.rgb(red: 255, green: 255, blue: 255)
         
         // embedding the send button within container view with a target
         containerView.addSubview(sendButton)
@@ -40,7 +40,7 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
         let searchBarBackground = UIView()
         containerView.addSubview(searchBarBackground)
         searchBarBackground.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: sendButton.leftAnchor, paddingTop: 10, paddingLeft: 22, paddingBottom: 10, paddingRight: 5, width: 0, height: 0)
-        searchBarBackground.layer.backgroundColor = UIColor.rgb(red: 255, green: 255, blue: 250).cgColor
+        searchBarBackground.layer.backgroundColor = UIColor.rgb(red: 255, green: 255, blue: 255).cgColor
         //searchBarBackground.layer.cornerRadius = 15
         
         // send button should be above message field in order to have it not run over when typing int the text field
@@ -57,12 +57,12 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
         messageTextField.attributedPlaceholder = NSAttributedString(string:" What's up?", attributes:[NSAttributedString.Key.foregroundColor: UIColor(red: 110/255, green: 110/255, blue: 110/255, alpha: 1)])
         messageTextField.font = UIFont.systemFont(ofSize: 22)
         messageTextField.keyboardType = UIKeyboardType.default
-        messageTextField.layer.backgroundColor = UIColor.rgb(red: 255, green: 255, blue: 255).cgColor
-        messageTextField.layer.cornerRadius = 0
+        messageTextField.layer.backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230).cgColor
+        messageTextField.layer.cornerRadius = 13
         
         return containerView
     } ()
-    
+        /*
     let returnHomeButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "home_unselected"), for: .normal)
@@ -70,9 +70,11 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
         button.addTarget(self, action: #selector(handleReturnHome), for: .touchUpInside)
         return button
     } ()
+    */
     
     let messageTextField: UITextField = {
         let tf = UITextField()
+         tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
         return tf
     } ()
     
@@ -88,22 +90,24 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        formValidation()
+        
         // Do any additional setup after loading the view.
         
         //view.backgroundColor = UIColor.rgb(red: 255, green: 255, blue: 255)
         
         
         collectionView?.register(ChatroomCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        collectionView?.backgroundColor = UIColor.rgb(red: 255, green: 0, blue: 0)
-                
+        collectionView?.backgroundColor = UIColor.rgb(red: 255, green: 255, blue: 255)
+         
+        observeMessages()
+        
         configureNavigationBar()
         
         setupToHideKeyboardOnTap()
         
         configureNavigationBar()
-        
-        observeMessages()
         
     }
 
@@ -128,32 +132,87 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
         return true
     }
     
+    
+    // MARK: - UICollectionView
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+        
+        //return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var height: CGFloat = 80
+        
+        let message = messages[indexPath.item]
+        
+        height = estimateFrameForText(message.messageText).height + 20
+        
+        return CGSize(width: view.frame.width, height: height)
+    }
+    
+    /*
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if messages.count > 4 {
+            if indexPath.item == messages.count - 1 {
+            observeMessages()   // THIS COULD CAUSE A LOOP
+            }
+        }
+    }
+   */
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ChatroomCell
+        
+        cell.message = messages[indexPath.item]
+        
+        configureMessage(cell: cell, message: messages[indexPath.item])
+        
+        return cell
+    }
+    
+    
+    // MARK: - Handlers
+    
+        @objc func handleSend() {
+        uploadMessageToServer()
+        
+        messageTextField.text = nil
+        
+        formValidation()
+    }
+    
     func estimateFrameForText(_ text: String) -> CGRect {
         let size = CGSize(width: 200, height: 1000)
         let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
         return NSString(string: text).boundingRect(with: size, options: options, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)], context: nil)
     }
     
-    @objc func handleReturnHome() {
+    
 
-        self.dismiss(animated:true, completion: nil)
-    }
-    
-    @objc func handleSend() {
-    
-        uploadMessageToServer()
-        
-        messageTextField.text = nil
-        
-        
-    }
     
     // MARK: - API
     
+    func configureMessage(cell: ChatroomCell, message: ChatroomMessage) {
+       // guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        
+        // accounting for the width of the message bubble based on text
+        cell.bubbleWidthAnchor?.constant = estimateFrameForText(message.messageText).width + 32
+        
+        // accounting for the height of the message bubble based on text
+        cell.frame.size.height = estimateFrameForText(message.messageText).height + 20
+        
+        cell.bubbleViewLeftAnchor?.isActive = true
+        cell.bubbleView.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
+        cell.textView.textColor = .black
+        cell.profileImageView.isHidden = false
+        
+    }
+    
     
     func uploadMessageToServer() {
-        print("CAN I STILL see the group identifier \(groupId)")
-        
+    
         guard let messageText = messageTextField.text else { return }
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
         
@@ -177,31 +236,14 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
         
     }
     
-    func configureMessage(cell: ChatroomCell, message: ChatroomMessage) {
-        guard let currentUid = Auth.auth().currentUser?.uid else { return }
-        
-        // accounting for the width of the message bubble based on text
-        cell.bubbleWidthAnchor?.constant = estimateFrameForText(message.messageText).width + 32
-        
-        // accounting for the height of the message bubble based on text
-        cell.frame.size.height = estimateFrameForText(message.messageText).height + 20
-        
-        cell.bubbleViewLeftAnchor?.isActive = true
-        cell.bubbleView.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
-        cell.textView.textColor = .black
-        cell.profileImageView.isHidden = false
-        
-    }
-    
+
+/*
     func observeMessages() {
-        
-        guard let currentUid = Auth.auth().currentUser?.uid else { return }
-        
         
         if currentKey == nil {
             DataService.instance.REF_GROUP_MESSAGES.child(groupId).queryLimited(toLast: 5).observeSingleEvent(of: .value, with: { (snapshot) in
                 
-                self.collectionView?.refreshControl?.endRefreshing()
+                //self.collectionView?.refreshControl?.endRefreshing()
                 
                 guard let first = snapshot.children.allObjects.first as? DataSnapshot else { return }
                 guard let allObjects = snapshot.children.allObjects as? [DataSnapshot] else { return }
@@ -236,61 +278,81 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
         
     }
     
+ */
+    
+    func observeMessages() {
+        
+        DataService.instance.REF_GROUP_MESSAGES.child(groupId).observe(.childAdded) { (snapshot) in
+            print(snapshot)
+            
+            let chatId = snapshot.key
+            
+            print("Just printing the chat key \(chatId)")
+            self.fetchMessage(withChatId: chatId, groupIdentifier: self.groupId)
+            
+            
+        }
+    }
+    
+    
     
     func fetchMessage(withChatId chatId: String, groupIdentifier: String) {
         
+        /*
         Database.fetchChatroomMessages(with: chatId, groupId: groupIdentifier) { (chat) in
             
             self.messages.append(chat)
+           
             
-
             self.messages.sort(by: { (chat1, chat2) -> Bool in
-                return chat1.creationDate < chat2.creationDate
+                return chat2.creationDate > chat1.creationDate
             })
+            self.collectionView?.reloadData()
+        }
+        */
+        
+        DataService.instance.REF_GROUP_MESSAGES.child(groupId).child(chatId).observeSingleEvent(of: .value) { (snapshot) in
+        
+            guard let dictionary = snapshot.value as? Dictionary<String, AnyObject> else { return }
+            guard let fromId = dictionary["fromId"] as? String else { return }  // Parsing through JSON data.
             
+            print("THIS IS THE OWNER ID \(fromId)")
+            
+            let message = ChatroomMessage(dictionary: dictionary)
+            self.messages.append(message)
             self.collectionView?.reloadData()
             
+            /*
+            Database.fetchUser(with: fromId, completion: { (user) in
+                
+                
+                let chat = ChatroomMessage(dictionary: dictionary)
+                
+                completion(chat)
+            })
+            */
+        }  
+    }
+    
+    
+    /*
+    func fetchMessage(withChatId chatId: String, groupIdentifier: String) {
+        
+        DataService.instance.REF_GROUP_MESSAGES.child(groupId).child(chatId).observeSingleEvent(of: .value) { (snapshot) in
+        
+            guard let dictionary = snapshot.value as? Dictionary<String, AnyObject> else { return }
+            guard let fromId = dictionary["fromId"] as? String else { return }  // Parsing through JSON data.
+            
+            print("THIS IS THE OWNER ID \(fromId)")
+            
+            let chat = ChatroomMessage(dictionary: dictionary)
+            self.messages.append(chat)
+             self.collectionView?.reloadData()
+            
         }
     }
-    
-    
-    // MARK: - UICollectionView
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return messages.count
-        
-        //return 5
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        var height: CGFloat = 80
-        
-        let message = messages[indexPath.item]
-        
-        height = estimateFrameForText(message.messageText).height + 20
-        
-        return CGSize(width: view.frame.width, height: height)
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if messages.count > 4 {
-            if indexPath.item == messages.count - 1 {
-            observeMessages()
-            }
-        }
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ChatroomCell
-        
-        cell.message = messages[indexPath.item]
-        
-        configureMessage(cell: cell, message: messages[indexPath.item])
-        
-        return cell
-    }
-    
+    */
+
     func configureNavigationBar() {
         
         navigationController?.navigationBar.isHidden = false
@@ -302,6 +364,21 @@ class ChatroomVC: UICollectionViewController, UICollectionViewDelegateFlowLayout
         //navigationItem.title = user.username
         navigationItem.title = "Chatroom"
 
+    }
+    
+    @objc func formValidation() {
+        guard
+            
+            messageTextField.hasText == true else {
+        
+                sendButton.isEnabled = false
+                sendButton.tintColor = UIColor(red: 80/255, green: 180/255, blue: 180/255, alpha: 1)
+                
+                return
+        }
+        
+        sendButton.isEnabled = true
+        sendButton.tintColor = UIColor.actionRed()
     }
 
 }
